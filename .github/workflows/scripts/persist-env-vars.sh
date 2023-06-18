33ffc1b8d180eab5754b5c733aeed615a0fb486f
Heshan Kumarasinghe <heshan.kumarasinghe@outlook.com>
2023-06-18 23:15:20 +0530

    add a script to persist env vars

--- .github/workflows/scripts/persist-env-vars.sh
+++ .github/workflows/scripts/persist-env-vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "export DB_NAME=${{ secrets.DB_NAME }}" >> ~/.bashrc
-echo "export DB_PASSWORD=${{ secrets.DB_PASSWORD }}" >> ~/.bashrc
-echo "export DB_USER=${{ secrets.DB_USER }}" >> ~/.bashrc
-echo "export JWT_SECRET=${{ secrets.JWT_SECRET }}" >> ~/.bashrc
-echo "export JWT_EXPIRES_IN=${{ secrets.JWT_EXPIRES_IN }}" >> ~/.bashrc
+echo "export DB_NAME=$1" >> ~/.bashrc
+echo "export DB_PASSWORD=$2" >> ~/.bashrc
+echo "export DB_USER=$3" >> ~/.bashrc
+echo "export JWT_SECRET=$4" >> ~/.bashrc
+echo "export JWT_EXPIRES_IN=$5" >> ~/.bashrc
