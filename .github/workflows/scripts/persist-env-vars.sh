11a8020256acb2a74c72b8cb363251bd6c4fd529
Heshan Kumarasinghe <heshan.kumarasinghe@outlook.com>
2023-06-19 00:08:22 +0530

    once again tweak

--- .github/workflows/scripts/persist-env-vars.sh
+++ .github/workflows/scripts/persist-env-vars.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo "export DB_NAME=$1" >> ~/.bashrc
-echo "export DB_PASSWORD=$2" >> ~/.bashrc
-echo "export DB_USER=$3" >> ~/.bashrc
-echo "export JWT_SECRET=$4" >> ~/.bashrc
-echo "export JWT_EXPIRES_IN=$5" >> ~/.bashrc
+echo "export PORT=$1" >> ~/.bashrc
+echo "export DB_CONNECTION_STRING=$2" >> ~/.bashrc
+echo "export DB_NAME=$3" >> ~/.bashrc
+echo "export DB_PASSWORD=$4" >> ~/.bashrc
+echo "export DB_USER=$5" >> ~/.bashrc
+echo "export JWT_SECRET=$6" >> ~/.bashrc
+echo "export JWT_EXPIRES_IN=$7" >> ~/.bashrc
